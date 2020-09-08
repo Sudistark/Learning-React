5e75ade5d0e63a18f31d96d5ee25e27608e3ddc4
sudistark <sudhanshur705@gmail.com>
2020-09-08 19:34:55 +0530

    Conditional output using ternary operator

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,19 +2,29 @@ import React from 'react';
 
 function Ninjas (props){
 
-    const NinjaList = props.ninjas.map(ninja =>{
-        if (ninja.age > 23){
-         return (
-          <div className="ninja" key={ninja.id}>
-               <div>Name: {ninja.name}</div>
-               <div>Age: {ninja.age}</div>
-               <div>Belt: {ninja.belt}</div>
-          </div>
-        )
-    }
-         else {
-           return null
-        }
+    //const NinjaList = props.ninjas.map(ninja =>{
+    //     if (ninja.age > 23){
+    //      return (
+    //       <div className="ninja" key={ninja.id}>
+    //            <div>Name: {ninja.name}</div>
+    //            <div>Age: {ninja.age}</div>
+    //            <div>Belt: {ninja.belt}</div>
+    //       </div>
+    //     )
+    // }
+    //      else {
+    //        return null
+    //     }
+    // })
+
+    const NinjaList = props.ninjas.map(ninja => {
+        return ninja.age > 23 ? (
+                <div className="ninja" key={ninja.id}>
+                   <div>Name: {ninja.name}</div>
+                   <div>Age: {ninja.age}</div>
+                   <div>Belt: {ninja.belt}</div>
+                </div>
+        ) : null;
     })
 
     return(
