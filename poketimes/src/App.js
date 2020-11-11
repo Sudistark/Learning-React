cbcad53e2ffe6c7931983317fc892d6dd98aaaf7
sudistark <sudhanshur705@gmail.com>
2020-11-11 21:06:54 +0530

    #32 axios,promises,request params

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
+import Post from './components/Post'
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
         <Navbar/>
         <Route exact path='/' component={Home}/>
          {/*Had to use exact because it was loading all the 3 components in the same page one below another*/}
-        <Route path='/contacts.html' component={Contact}/>
+        <Route path='/contact.html' component={Contact}/>
         <Route path='/about.html' component={About}/>
+        <Route path='/:postId' component={Post}/>
       </div>
     </BrowserRouter>
   );
