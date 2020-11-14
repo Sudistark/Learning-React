3b0f371a324ae3e60100e3df1f7f2ed50229fb44
sudistark <sudhanshur705@gmail.com>
2020-11-14 08:49:42 +0530

    Updates

---
+++ precache-manifest.35577bed8efab72b3d556b4b12a3582c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de63b12cf76c0642e95af266939b839",
+    "url": "/Learning-React/todoapp/index.html"
+  },
+  {
+    "revision": "f6a755c9615afdf45905",
+    "url": "/Learning-React/todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b895918896e9ccdf0a7f",
+    "url": "/Learning-React/todoapp/static/js/2.f293178a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Learning-React/todoapp/static/js/2.f293178a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a755c9615afdf45905",
+    "url": "/Learning-React/todoapp/static/js/main.38a155a3.chunk.js"
+  },
+  {
+    "revision": "38195d66a69991d7c5ef",
+    "url": "/Learning-React/todoapp/static/js/runtime-main.686ab736.js"
+  }
+]);
